4f78fac2cedc95a6a3485d07ef69a769ab7529e3
bjersgen <601059768@qq.com>
2022-08-27 14:53:32 +0800

    update 0827

--- src/unit_test/test_controlal.cpp
+++ src/unit_test/test_controlal.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 int main(){
-    
+
     controlal control(controlal::Mode::RealTime);
     rate Rate_command(CAM_RATE);
     while(1){
         if(control.control_done)control.sendCommand(3000,2000,0,0);
         Rate_command.sleep();
     }
-	return 0 ; 
+    return 0 ; 
 }
